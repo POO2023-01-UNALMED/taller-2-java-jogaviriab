959d15ecac48684d81a60a3b3529ce6bb38e8968
jogaviriab <124937403+jogaviriab@users.noreply.github.com>
2023-02-20 16:04:42 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,26 +1,56 @@
-package test;
+package Prueba;
 
 public class Auto {
 	String modelo;
 	int precio;
-	Asiento asientos;
+	Asiento[] asientos = new Asiento[20];
 	String marca;
 	Motor motor;
 	int registro;
-	int cantidadCreados;
+	static int cantidadCreados=0;
 	
-	int cantidadAsiento(){
+
+	public Auto(String string, int i, Motor motor1, String string2, Asiento asien1[], int j) {
+		this.modelo = string;
+		this.precio = i;
+		this.motor = motor1;
+		this.asientos = asien1;
+		this.marca = string2;
+		this.registro = j;
+		Auto.cantidadCreados = Auto.cantidadCreados +1;
+		
+	}
+	
+	int cantidadAsientos() {
+		int cant = 0;
+		for(int i =0;i<20;i++) {
+			if (this.asientos[i] != null) {
+				cant++;
+			}
+		}
+		return cant;
 		
 		
 	}
-
 	String verificarIntegridad() {
 		String msg;
-		if(this.registro == this.motor.registro && this.registro == this.asientos.registro ) {
-			msg = "Auto original";
-		}else {
+		if(this.registro != this.motor.registro) {
 			msg = "Las piezas no son originales";
+			return msg;
 		}
+		
+		
+		for(int i =0;i<20;i++) {
+			if (this.asientos[i] != null) {
+				if(this.asientos[i].registro!= this.registro) {
+					msg = "Las piezas no son originales";
+					return msg;
+				}
+			}
+		}
+		msg = "Auto original";
 		return msg;
+
 	}
+	
 }
