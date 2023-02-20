a65526dbc27e2113ef5d21df0f366a97884da0c1
jogaviriab <124937403+jogaviriab@users.noreply.github.com>
2023-02-20 16:05:01 -0500

    Update Asiento.java

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -1,10 +1,16 @@
-package test;
+package Prueba;
 
 public class Asiento {
 	String color;
 	int precio;
 	int registro;
 	
+	public Asiento(String string, int i, int j) {
+		this.color = string;
+		this.precio = i;
+		this.registro = j;
+	}
+
 	void cambiarColor(String color) {
 		if (color == "rojo"){
 			this.color = color;
